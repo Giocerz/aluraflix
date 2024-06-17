183857f6be8d83800428848a676ea8b23a43609f
giocerz <grce2000@gmail.com>
2024-06-16 21:27:32 -0500

    add: main banner

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,10 +1,21 @@
 import './Banner.css'
+import playerImage from '../../assets/player.svg';
 
 function Banner() {
     return (
         <section className='banner-section'>
             <div className='banner-content'>
-                <h1 className='banner-title'>FRONT END</h1>
+                <div className='banner-description'>
+                    <h1 className='banner-title'>FRONT END</h1>
+                    <h2>Challenge React</h2>
+                    <p>Este challenge es una forma de aprendizaje.
+                        Es un mecanismo donde podrás comprometerte
+                        en la resolución de un problema para poder
+                        aplicar todos los conocimientos adquiridos
+                        en la formación React.
+                    </p>
+                </div>
+                <img className='banner-image' src={playerImage}/>
             </div>
         </section>
     );
