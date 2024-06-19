797d97bf3c06d72f3553ec02520063d6e8214ccd
giocerz <grce2000@gmail.com>
2024-06-19 01:43:37 -0500

    fix: change btn colors when the current page changes

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,21 +2,37 @@ import './Header.css'
 import aluraLogo from '../../assets/LogoMain.svg'
 import newVideoIcon from '../../assets/NuevoVideo.svg'
 import homeIcon from '../../assets/HomeIcon.svg'
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
 
 function Header() {
+    const location = useLocation();
+    const [btnSelected, setBtnSelected] = useState(true);
+
+    useEffect(() => {
+        if(location.pathname == '/') {
+            setBtnSelected(true);
+        } else if(location.pathname == '/new') {
+            setBtnSelected(false);
+        }
+    }, [location.pathname]);
+
+    const styleBtnSelected = (selected) => {
+        return selected ? 'select-btn' : '';
+    }
+
     return (
         <>
             <header className="main-header">
                 <img className='header-logo' src={aluraLogo} />
                 <nav>
-                    <Link className='home-btn header-btn' to={'/'}>Home</Link>
-                    <Link className='new-btn header-btn' to={'/new'}>New</Link>
+                    <Link className={`home-btn header-btn ${styleBtnSelected(btnSelected)}`} to={'/'}>Home</Link>
+                    <Link className={`new-btn header-btn ${styleBtnSelected(!btnSelected)}`} to={'/new'}>New</Link>
                 </nav>
             </header>
             <header className="mobile-header">
                 <nav>
-                    <Link className='home-btn-mobile' to={'/'}><img src={homeIcon}/><spa>Home</spa></Link>
+                    <Link className='home-btn-mobile' to={'/'}><img src={homeIcon}/><span>Home</span></Link>
                     <Link className='new-btn-mobile' to={'/new'}><img src={newVideoIcon}/></Link>
                 </nav>
             </header>
